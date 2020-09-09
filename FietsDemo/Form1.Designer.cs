d5b30edb823a4d30e02245a572845b9c40121558
Samir <samir.rademakers2@gmail.com>
2020-09-09 12:47:36 +0200

    [Update] Layout from GUI fixed

--- FietsDemo/Form1.Designer.cs
+++ FietsDemo/Form1.Designer.cs
@@ -49,7 +49,7 @@
             this.Speed.AutoSize = true;
             this.Speed.BackColor = System.Drawing.Color.White;
             this.Speed.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Speed.Location = new System.Drawing.Point(259, 83);
+            this.Speed.Location = new System.Drawing.Point(300, 83);
             this.Speed.Name = "Speed";
             this.Speed.Size = new System.Drawing.Size(177, 55);
             this.Speed.TabIndex = 0;
@@ -60,7 +60,7 @@
             this.SpeedValue.AutoSize = true;
             this.SpeedValue.BackColor = System.Drawing.Color.White;
             this.SpeedValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SpeedValue.Location = new System.Drawing.Point(442, 83);
+            this.SpeedValue.Location = new System.Drawing.Point(483, 83);
             this.SpeedValue.Name = "SpeedValue";
             this.SpeedValue.Size = new System.Drawing.Size(0, 55);
             this.SpeedValue.TabIndex = 1;
@@ -70,7 +70,7 @@
             this.Heartrate.AutoSize = true;
             this.Heartrate.BackColor = System.Drawing.Color.White;
             this.Heartrate.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Heartrate.Location = new System.Drawing.Point(198, 148);
+            this.Heartrate.Location = new System.Drawing.Point(239, 150);
             this.Heartrate.Name = "Heartrate";
             this.Heartrate.Size = new System.Drawing.Size(238, 55);
             this.Heartrate.TabIndex = 2;
@@ -79,8 +79,9 @@
             // HeartrateValue
             // 
             this.HeartrateValue.AutoSize = true;
+            this.HeartrateValue.BackColor = System.Drawing.Color.White;
             this.HeartrateValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.HeartrateValue.Location = new System.Drawing.Point(442, 148);
+            this.HeartrateValue.Location = new System.Drawing.Point(483, 150);
             this.HeartrateValue.Name = "HeartrateValue";
             this.HeartrateValue.Size = new System.Drawing.Size(0, 55);
             this.HeartrateValue.TabIndex = 3;
@@ -90,7 +91,7 @@
             this.ElapsedTime.AutoSize = true;
             this.ElapsedTime.BackColor = System.Drawing.Color.White;
             this.ElapsedTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ElapsedTime.Location = new System.Drawing.Point(106, 218);
+            this.ElapsedTime.Location = new System.Drawing.Point(147, 219);
             this.ElapsedTime.Name = "ElapsedTime";
             this.ElapsedTime.Size = new System.Drawing.Size(330, 55);
             this.ElapsedTime.TabIndex = 4;
@@ -101,7 +102,7 @@
             this.ElapsedTimeValue.AutoSize = true;
             this.ElapsedTimeValue.BackColor = System.Drawing.Color.White;
             this.ElapsedTimeValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ElapsedTimeValue.Location = new System.Drawing.Point(442, 218);
+            this.ElapsedTimeValue.Location = new System.Drawing.Point(483, 219);
             this.ElapsedTimeValue.Name = "ElapsedTimeValue";
             this.ElapsedTimeValue.Size = new System.Drawing.Size(0, 55);
             this.ElapsedTimeValue.TabIndex = 5;
@@ -111,7 +112,7 @@
             this.DistanceTraveled.AutoSize = true;
             this.DistanceTraveled.BackColor = System.Drawing.Color.White;
             this.DistanceTraveled.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DistanceTraveled.Location = new System.Drawing.Point(12, 287);
+            this.DistanceTraveled.Location = new System.Drawing.Point(53, 287);
             this.DistanceTraveled.Name = "DistanceTraveled";
             this.DistanceTraveled.Size = new System.Drawing.Size(424, 55);
             this.DistanceTraveled.TabIndex = 6;
@@ -120,8 +121,9 @@
             // DistanceTraveledValue
             // 
             this.DistanceTraveledValue.AutoSize = true;
+            this.DistanceTraveledValue.BackColor = System.Drawing.Color.White;
             this.DistanceTraveledValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DistanceTraveledValue.Location = new System.Drawing.Point(442, 287);
+            this.DistanceTraveledValue.Location = new System.Drawing.Point(483, 287);
             this.DistanceTraveledValue.Name = "DistanceTraveledValue";
             this.DistanceTraveledValue.Size = new System.Drawing.Size(0, 55);
             this.DistanceTraveledValue.TabIndex = 7;
@@ -143,18 +145,18 @@
             this.Resistance.AutoSize = true;
             this.Resistance.BackColor = System.Drawing.Color.White;
             this.Resistance.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Resistance.Location = new System.Drawing.Point(174, 357);
+            this.Resistance.Location = new System.Drawing.Point(202, 355);
             this.Resistance.Name = "Resistance";
-            this.Resistance.Size = new System.Drawing.Size(262, 55);
+            this.Resistance.Size = new System.Drawing.Size(275, 55);
             this.Resistance.TabIndex = 9;
-            this.Resistance.Text = "Resistance";
+            this.Resistance.Text = "Resistance:";
             // 
             // ResistanceValue
             // 
             this.ResistanceValue.AutoSize = true;
             this.ResistanceValue.BackColor = System.Drawing.Color.White;
             this.ResistanceValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ResistanceValue.Location = new System.Drawing.Point(443, 357);
+            this.ResistanceValue.Location = new System.Drawing.Point(483, 357);
             this.ResistanceValue.Name = "ResistanceValue";
             this.ResistanceValue.Size = new System.Drawing.Size(0, 55);
             this.ResistanceValue.TabIndex = 10;
@@ -164,18 +166,18 @@
             this.accumalatedPower.AutoSize = true;
             this.accumalatedPower.BackColor = System.Drawing.Color.White;
             this.accumalatedPower.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.accumalatedPower.Location = new System.Drawing.Point(-11, 431);
+            this.accumalatedPower.Location = new System.Drawing.Point(12, 421);
             this.accumalatedPower.Name = "accumalatedPower";
-            this.accumalatedPower.Size = new System.Drawing.Size(452, 55);
+            this.accumalatedPower.Size = new System.Drawing.Size(465, 55);
             this.accumalatedPower.TabIndex = 11;
-            this.accumalatedPower.Text = "Accumalated Power";
+            this.accumalatedPower.Text = "Accumalated Power:";
             // 
             // APLabel
             // 
             this.APLabel.AutoSize = true;
             this.APLabel.BackColor = System.Drawing.Color.White;
             this.APLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.APLabel.Location = new System.Drawing.Point(448, 431);
+            this.APLabel.Location = new System.Drawing.Point(483, 421);
             this.APLabel.Name = "APLabel";
             this.APLabel.Size = new System.Drawing.Size(0, 55);
             this.APLabel.TabIndex = 12;

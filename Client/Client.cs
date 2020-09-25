3aff76345619b7f15e27d48b5c2e35167ab7469a
Rik <rik.vos01@gmail.com>
2020-09-25 16:04:05 +0200

    [Added] RSA client and construction message protocol

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Client;
+using System;
 using System.IO;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading;
 
 namespace GameClient
@@ -11,16 +14,19 @@ namespace GameClient
         static void Main(string[] args)
         {
             Client client = new Client();
+
         }
 
         private String username;
         private TcpClient server;
         private StreamWriter streamWriter;
         private StreamReader streamReader;
+        private RSAClient rsaClient;
 
 
         public Client()
         {
+            this.rsaClient = new RSAClient(2048);
 
             Console.Write("Username: ");
             this.username = Console.ReadLine();
@@ -60,7 +66,7 @@ namespace GameClient
                 this.streamWriter.Flush();
             }
             catch { }
-            
+
         }
 
         public void ReadTextMessage()
@@ -69,12 +75,65 @@ namespace GameClient
             {
                 try
                 {
-                 Console.WriteLine(this.streamReader.ReadLine());                }
+                    Console.WriteLine(this.streamReader.ReadLine());
+                }
                 catch
                 {
                     break;
                 }
             }
+        }
+
+        public string getUpdateMessageString(int session, int heartrate, double accDistance, double speed, double instPower, double accPower)
+        {
+            int checksum = 0;
+            dynamic json = new
+            {
+                Session = session,
+                data = new
+                {
+                    HeartRate = heartrate,
+                    AccumulatedDistance = accDistance,
+                    Speed = speed,
+                    InstantaniousPower = instPower
+                },
+                Checksum = checksum
+            };
+
+            return JsonSerializer.Serialize(json);
+        }
+
+        public string getMessageString(int session, string message)
+        {
+            int checksum = 0;
+            dynamic json = new
+            {
+                Session = session,
+                data = new
+                {
+                    Message = message
+                },
+                Checksum = checksum
+            };
+
+            return JsonSerializer.Serialize(json);
+        }
+
+        public string getRequestMessageString(byte[] publicKey, byte[] exponent)
+        {
+            int checksum = 0;
+            dynamic json = new
+            {
+                Type = "request",
+                Data = new
+                {
+                    PublicKey = publicKey,
+                    Exponent = exponent
+                },
+                Checksum = checksum
+            };
+
+            return JsonSerializer.Serialize(json);
         }
     }
 }
---
+++ Client/RSAClient.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Client
+{
+    class RSAClient
+    {
+        private RSAParameters parameters;
+        private RSACryptoServiceProvider rsa;
+
+        public RSAClient(int size)
+        {
+            this.rsa = new RSACryptoServiceProvider(size);
+        }
+
+        public string encryptMessage(string message)
+        {
+            return rsa.
+        }
+
+        public void setKey(byte[] key, byte[] exponent)
+        {
+            this.parameters = new RSAParameters();
+            parameters.Modulus = key;
+            parameters.Exponent = exponent;
+        }
+
+    }
+}

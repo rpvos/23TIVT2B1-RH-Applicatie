3907f57f98ff372f10dc9106d89dedd7d3d0a036
Rik <rik.vos01@gmail.com>
2020-10-12 14:18:04 +0200

    [added] microsoft way of encryption

--- SharedItems/Crypto.cs
+++ SharedItems/Crypto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Http.Headers;
 using System.Net.Sockets;
 using System.Security.Cryptography;
 using System.Text;
@@ -9,10 +10,7 @@ namespace SharedItems
 {
     public class Crypto
     {
-        /// <summary>
-        /// object that keeps the key
-        /// </summary>
-        private Rijndael rijndael;
+
 
         public CryptoStream encryptionCryptoStream { get; }
         public CryptoStream decryptionCryptoStream { get; }
@@ -22,9 +20,9 @@ namespace SharedItems
         private string totalBuffer;
 
         private Action<string> handleMethod;
+        private byte[] key;
+        private byte[] iV;
 
-        private MemoryStream encryptionMemoryStream;
-        private MemoryStream decryptionMemoryStream;
 
         /// <summary>
         /// Constructor wwith an method passed in to handle the incoming data
@@ -38,30 +36,103 @@ namespace SharedItems
             this.networkStream = networkStream;
             this.handleMethod = handleMethod;
 
-            //this.encryptionMemoryStream = new MemoryStream();
-           //this.decryptionMemoryStream = new MemoryStream();
-            //this.rijndael = Rijndael.Create();
 
-            //rijndael.Key = new byte[] { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16 };
-            //rijndael.IV = new byte[] { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16 };
-
-           // this.encryptionCryptoStream = new CryptoStream(encryptionMemoryStream, rijndael.CreateEncryptor(), CryptoStreamMode.Write);
-           // this.decryptionCryptoStream= new CryptoStream(decryptionMemoryStream, rijndael.CreateDecryptor(), CryptoStreamMode.Read);
+            this.key = new byte[] { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16 };
+            this.iV = new byte[] { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16 };
 
             networkStream.BeginRead(buffer, 0, buffer.Length, new AsyncCallback(OnRead), null);
         }
+
+        private string DecryptStringFromBytes(byte[] cipherText)
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (key == null || key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (iV == null || iV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Rijndael object
+            // with the specified key and IV.
+            using (Rijndael rijAlg = Rijndael.Create())
+            {
+                rijAlg.Key = key;
+                rijAlg.IV = iV;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+        private byte[] EncryptStringToBytes(string plainText)
+        {
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (key == null || key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (iV == null || iV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+            // Create an Rijndael object
+            // with the specified key and IV.
+            using (Rijndael rijAlg = Rijndael.Create())
+            {
+                rijAlg.Key = key;
+                rijAlg.IV = iV;
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+        }
+
 
         #region stream dynamics
 
         public void WriteTextMessage(string message)
         {
-            byte[] dataAsBytes = Encoding.UTF8.GetBytes(message + "\r\n\r\n");
+            message += "\r\n\r\n";
+            byte[] dataAsBytes = EncryptStringToBytes(message);
 
-            //encryptionCryptoStream.Write(dataAsBytes,0,dataAsBytes.Length);
-            //encryptionCryptoStream.Clear();
-
-            //var array = encryptionMemoryStream.ToArray();
-            
             networkStream.Write(dataAsBytes, 0, dataAsBytes.Length);
             networkStream.Flush();
         }
@@ -70,20 +141,11 @@ namespace SharedItems
         {
             try
             {
-                // todo encrypted data has been gotten but we cant decode it
                 int receivedBytes = networkStream.EndRead(ar);
-                
 
-                //decryptionMemoryStream.Write(buffer,0,buffer.Length);
+                string receivedText = DecryptStringFromBytes(buffer);
 
-                //byte[] decrypted = new byte[1024];
-                //decryptionCryptoStream.Read(decrypted,0,receivedBytes);
-
-                //Console.WriteLine(decrypted);//todo remove
-
-                string receivedText = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
-
-                //Console.WriteLine(receivedText);//todo remove
+                Console.WriteLine(receivedText);
 
                 totalBuffer += receivedText;
             }

faff982f031020003996b5dd3d4358757cac4d9d
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2020-10-16 15:46:24 +0200

    [Added] new loginView and viewModel

---
+++ WPFDoctorApplication/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WPFDoctorApplication.Utils;
+
+namespace WPFDoctorApplication.ViewModels
+{
+    public class LoginViewModel : MyObservableObject
+    {
+        public LoginViewModel()
+        {
+
+        }
+    }
+}

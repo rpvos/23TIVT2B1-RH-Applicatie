3aa5e97ad852699a69a7dc8874b62a4c405f3b20
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2020-10-19 00:57:57 +0200

    [Update] Working speed graph

--- WPFDoctorApplication/Models/PatientBike.cs
+++ WPFDoctorApplication/Models/PatientBike.cs
@@ -1,8 +1,12 @@
 ﻿using GalaSoft.MvvmLight.Command;
+using LiveCharts;
+using LiveCharts.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using WPFDoctorApplication.Utils;
 
@@ -16,9 +20,24 @@ namespace WPFDoctorApplication.Models
     public class PatientBike : CustomObservableObject
     {
         private DoctorClient doctorClient;
+        private double _speed;
+
+        //This has to change for performance
+        private bool IsReading = true;
+
+        public double Speed
+        {
+            get { return _speed; }
+            set
+            { 
+                _speed = value;
+                //SpeedValues.RemoveAt(0);
+                //SpeedValues.Add(value);
+            }
+        }
 
         public string Username { get; set; }
-        public double Speed { get; set; }
+        //public double Speed { get; set; }
         public double DistanceTraveled { get; set; }
         public double AccumulatedPower { get; set; }
         public double ElapsedTime { get; set; }
@@ -28,20 +47,59 @@ namespace WPFDoctorApplication.Models
         public ICommand PrivateChatKeyDownCommand { get; set; }
 
         public string PrivateChatMessage { get; set; }
-
+        public SeriesCollection SpeedCollection { get; set; }
+        public string[] SpeedLabels { get; set; }
+        public Func<double, string> SpeedYFormatter { get; set; }
+        public ChartValues<double> SpeedValues { get; set; }
         public PatientBike(DoctorClient doctorClient, string username)
         {
             this.Username = username;
             PrivateChatList = new ObservableCollection<string>();
             PrivateChatKeyDownCommand = new RelayCommand(() => PrivateChatKeyDown()); ;
             this.doctorClient = doctorClient;
+
+            InitializeGraphs();
         }
+    
 
         private void PrivateChatKeyDown()
         {
             PrivateChatList.Add("Doctor: " + PrivateChatMessage);
             doctorClient.sendPrivMessage(PrivateChatMessage, Username);
             PrivateChatMessage = "";
+        }
+
+        private void InitializeGraphs()
+        {
+            SpeedValues = new ChartValues<double> { 0, 0, 0, 0, 0 };
+            SpeedCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Speed",
+                    Values = SpeedValues
+                }
+            };
+
+            SpeedLabels = new[] { "?", "?", "?", "?", "?" };
+            //SpeedYFormatter = value => value.ToString();
+
+            Task.Factory.StartNew(Read);
+        }
+
+        private void Read()
+        {
+
+            while (IsReading)
+            {
+                Thread.Sleep(250);
+
+                SpeedValues.Add(Speed);
+
+                //lets only use the last 30 values
+                if (SpeedValues.Count > 30) 
+                    SpeedValues.RemoveAt(0);
+            }
         }
     }
 }

efa0d6a3291da9a0336b5e96be1efb406b2f2d55
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2020-10-24 14:25:43 +0200

    [Added] emergency stop

--- WPFDoctorApplication/ViewModels/PatientViewModel.cs
+++ WPFDoctorApplication/ViewModels/PatientViewModel.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 using WPFDoctorApplication.Models;
 using WPFDoctorApplication.Utils;
 using WPFDoctorApplication.Views;
@@ -17,16 +19,25 @@ namespace WPFDoctorApplication.ViewModels
         //public double Speed { get {return PatientBike.Speed; } set { Speed = value; } }
 
         public string Username { get; set; }
+        public ICommand StopCommand{ get; set; }
         public PatientViewModel(PatientBike patientBike, DoctorClient doctorClient)
         {
             this.PatientBike = patientBike;
             this.doctorClient = doctorClient;
             this.Speed = patientBike.Speed;
             this.Username = patientBike.Username;
+
+            StopCommand = new RelayCommand(EmergencyStop);
         }
 
         public void SendResistance()
         {
+            doctorClient.sendResistance(PatientBike.ResistanceValue + "", PatientBike.Username);
+        }
+
+        private void EmergencyStop()
+        {
+            PatientBike.ResistanceValue = 100;
             doctorClient.sendResistance(PatientBike.ResistanceValue + "", PatientBike.Username);
         }
     }
